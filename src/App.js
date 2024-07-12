5fda9714a77f1f9d37216af219dd792df9f90821
Ravish Ranjan <122588009+Ravish-Ranjan@users.noreply.github.com>
2024-07-12 14:32:37 +0530

    project itration 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Barcode from "react-barcode";
+import TextField from "@mui/material/TextField";
+import "./app.css";
+import { Typography } from "@mui/material";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [value, setValue] = useState("");
+	const [config, setCol] = useState({
+		lineColor: "#000000",
+		background: "#ffffff",
+	});
+	return (
+		<div className="card">
+			<Typography variant="h4" className="heading">Barcode Generator</Typography>
+			<div
+				style={{
+					width: "100%",
+					height: "auto",
+					overflowX: "auto",
+					display: "grid",
+					placeItems: "center",
+					outline: "1px solid black",
+					borderRadius: ".5rem",
+				}}
+			>
+				<Barcode value={value === "" ? "barcode" : value} {...config} className="code" />
+			</div>
+			<TextField
+				label="Enter Barcode Value"
+				value={value}
+				onChange={(e) => {
+					setValue(e.target.value);
+				}}
+			/>
+			<div className="inline">
+				<TextField
+					value={config.lineColor}
+					type="color"
+					label="Line Color"
+					className="color"
+					onChange={(e) => {
+						setCol({ ...config, lineColor: e.target.value });
+					}}
+				/>
+				<TextField
+					value={config.background}
+					type="color"
+					label="Background Color"
+					className="color"
+					onChange={(e) => {
+						setCol({ ...config, background: e.target.value });
+					}}
+				/>
+			</div>
+		</div>
+	);
 }
 
 export default App;
